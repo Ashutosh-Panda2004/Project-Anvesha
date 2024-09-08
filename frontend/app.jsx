903ba52fdb8c58aecf009fdb8fc1a6c1a5e7c9a1
Ashutosh-Panda2004 <ashutosh2004.panda@gmail.com>
2024-09-08 23:46:22 +0530

    Adding app.jsx with only homePage component

---
+++ frontend/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./Components/homePage/homepage";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
