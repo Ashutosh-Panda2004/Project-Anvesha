807250e863a4637bbd099c52ebf4a79d764a9789
Ashutosh-Panda2004 <ashutosh2004.panda@gmail.com>
2024-09-09 11:25:17 +0530

    Pushing feature into the app.jsx

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomePage from "./Components/homePage/homepage";
+import LoginSignup from "./Components/login_signup/login_signup";
 
 function App() {
   return (
     <div>
       <HomePage />
+      <LoginSignup/>
     </div>
   );
 }
