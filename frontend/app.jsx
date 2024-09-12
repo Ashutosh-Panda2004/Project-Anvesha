4e4dae2b004bd9650dfb49045dfa8b202a3104ab
Ashutosh-Panda2004 <ashutosh2004.panda@gmail.com>
2024-09-12 08:50:31 +0530

    Modifying app.jsx, visualising text-editor

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "./Components/homePage/homepage";
 import LoginSignup from "./Components/login_signup/login_signup";
+import TextEditor from "./Components/login_signup/text_editor";
 
 function App() {
   return (
     <div>
       <HomePage />
       <LoginSignup/>
+      <TextEditor/>
     </div>
   );
 }
